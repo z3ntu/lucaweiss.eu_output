7d193f29cec62f884a546dd53ee20c2cf0f26e44
z3ntu <WEI16416@spengergasse.at>
2015-07-18 18:16:56 +0200

    youtubesubscriptions update, should look much better now!

--- YoutubeSubscriptions/youtubesubscriptions.js
+++ YoutubeSubscriptions/youtubesubscriptions.js
@@ -40,7 +40,8 @@ function handleResponse(response) {
     console.log(response);
     console.log(downloadArray);
     if(typeof response.nextPageToken === 'undefined'){
-      console.log("last one :(");
+      $('#spinTarget').spin(false);
+      console.log("spinner should stop; last one :(");
       $('#downloadA').attr("download", "youtubesubscriptions.json");
       $('#downloadA').attr('href', "data:application/json,"+JSON.stringify(downloadArray));
       $('#downloadA').show();
@@ -56,28 +57,8 @@ function foreachCallback(key, value) {
 }
 
 function spin() {
-  var opts = {
-  lines: 13 // The number of lines to draw
-, length: 28 // The length of each line
-, width: 14 // The line thickness
-, radius: 42 // The radius of the inner circle
-, scale: 1 // Scales overall size of the spinner
-, corners: 1 // Corner roundness (0..1)
-, color: '#fff' // #rgb or #rrggbb or array of colors
-, opacity: 0.25 // Opacity of the lines
-, rotate: 0 // The rotation offset
-, direction: 1 // 1: clockwise, -1: counterclockwise
-, speed: 1 // Rounds per second
-, trail: 60 // Afterglow percentage
-, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-, zIndex: 2e9 // The z-index (defaults to 2000000000)
-, className: 'spinner' // The CSS class to assign to the spinner
-, top: '50%' // Top position relative to parent
-, left: '50%' // Left position relative to parent
-, shadow: false // Whether to render a shadow
-, hwaccel: false // Whether to use hardware acceleration
-, position: 'absolute' // Element positioning
-}
-var target = document.getElementById('spinTarget')
-var spinner = new Spinner(opts).spin(target);
+// var target = document.getElementById('spinTarget')
+// var spinner = new Spinner(opts).spin(target);
+// $('#spinTarget').spin('small', '#fff');
+$('#spinTarget').spin( {lines: 8, length: 4, width: 3, radius: 5, position: 'relative' });
 }
